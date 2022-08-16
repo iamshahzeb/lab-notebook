c340392edf6918adf874a0c89bd6d9da74d31088
iamshahzeb <syedshahzebakhtar@gmail.com>
2022-08-17 03:52:45 +0500

    chore: document title should be updated when user navigates to other routes.
    re#LN-0017

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 // Packages
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { BrowserRouter } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
 // Components
@@ -11,31 +12,41 @@ import { AppRoutes } from './components/routes';
 import 'react-toastify/dist/ReactToastify.css';
 
 // Services
+import { generalUtilService } from './services/general';
 import { reactQueryUtilService } from './services/react-query';
 import './services/translations';
+import { routeUtilService } from './services/routes';
 
 // Initialize react query client
 const queryClient = new QueryClient(reactQueryUtilService.queryDefaultConfig);
 
 const App = () => {
+  /**
+  * @Hooks
+  */
+  const location = useLocation();
+
+  // Setting window title as per route
+  useEffect(() => {
+    generalUtilService.setPageTitle(location.pathname, routeUtilService.applicationRoutes);
+  }, [location.pathname]);
+
   return (
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          newestOnTop={false}
-          closeOnClick
-          rtl={false}
-          pauseOnFocusLoss
-          draggable
-          pauseOnHover
-        />
-        <AppRoutes />
-        <ReactQueryDevtools initialIsOpen={false} />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <AppRoutes />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
